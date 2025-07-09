3fbc33ffcfec12f5c863921ed4e84fa4cd76f07f
Stephane Mfuni <stephuni35@gmail.com>
2025-07-09 19:17:37 +0300

    fix prisma generated files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ["app/generated/prisma/**/*", "node_modules/**/*", ".next/**/*"],
+    ignores: ["lib/generated/prisma/**/*", "node_modules/**/*", ".next/**/*"],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
